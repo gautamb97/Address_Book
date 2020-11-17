d23e4ed5553e622c1685a18484c51a297ba955a1
icode32 <www.gautam971997@gmail.com>
2020-11-17 07:55:54 +0530

    Added:Added_contact_and_checked_for_existing_contact

--- AddressBook.java
+++ AddressBook.java
@@ -10,6 +10,22 @@ public class AddressBook {
 			friends = new Contact[10];
 			numfriends = 0;
 	    }
+	 // Add a contact that's passed in as a parameter.
+	    public void addContact(Contact c) {
+			friends[numfriends] = c;
+			numfriends++;
+	    }
+
+	    // Print out info on all contacts using method Contact class.
+	    public void printContacts() {
+			for (int i=0;i<numfriends;i++)
+		    	friends[i].printContact();
+	    }
+
+	    // Returns the number of friends currently in AddressBook
+	    public int numContacts() {
+			return numfriends;
+	    }
 	public static void main(String[] args) {
 		System.out.println("Welcome To Address Book");
 	}
