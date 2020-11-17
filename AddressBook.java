152fe8fcbe97900684ad1666558cf43f0663d67b
icode32 <www.gautam971997@gmail.com>
2020-11-17 07:49:06 +0530

    Added:Created_an_empty_address_book

--- AddressBook.java
+++ AddressBook.java
@@ -2,6 +2,14 @@ package addressbook;
 
 
 public class AddressBook {
+	 private Contact[] friends; // An array of Contacts - each stores info for one friend
+	    private int numfriends; // Number of friends currently in AddressBook
+
+	    // Create an empty AddressBook
+	    public AddressBook() {
+			friends = new Contact[10];
+			numfriends = 0;
+	    }
 	public static void main(String[] args) {
 		System.out.println("Welcome To Address Book");
 	}
